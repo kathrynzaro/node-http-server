5b8916492f19c2ab7e1f3fddb33f66b276f0a13b
kathrynzaro <zarokathryn@gmail.com>
2022-09-13 14:12:49 -0700

    adds get route to /cats

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-import http from "node:http";
-import app from "./lib/app.js";
+import http from 'node:http';
+import app from './lib/app.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.APP_PORT || 8080;
+const hostname = process.env.APP_HOST || 'localhost';
 
-server.listen(PORT, () => {
+server.listen(PORT, hostname, () => {
   console.log(server.address());
 });
+console.log('Listening...');
